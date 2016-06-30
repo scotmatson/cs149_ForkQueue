6bb0ceaef0af45db30fafb1d7e1c6bec30bb5758
Francisco McGee <fmcgee@natera.com>
2016-06-30 12:58:23 -0700

    inserted troubleshooting code in HPFP_preemptive and HPFP_Queue

--- src/schedulingAlgorithms/HPFP_Queue.java
+++ src/schedulingAlgorithms/HPFP_Queue.java
@@ -7,10 +7,13 @@ import java.util.PriorityQueue;
 /**
  * Created by danieltam on 6/27/16.
  */
-public class HPFP_Queue extends ArrayList{
+public class HPFP_Queue extends ArrayList
+{
 
-    public HPFP_Queue(int priorityQueueCount){
-        while (priorityQueueCount > 0) {
+    public HPFP_Queue(int priorityQueueCount)
+    {
+        while (priorityQueueCount > 0)
+        {
             PriorityQueue<Task> priorityQueue = new PriorityQueue<>(10, new Comparator<Task>()
             {
                 public int compare(Task t1, Task t2)
@@ -21,17 +24,18 @@ public class HPFP_Queue extends ArrayList{
             this.add(priorityQueue);
             priorityQueueCount -= 1;
         }
+        System.out.println("SIZE OF ACTUAL READY QUEUE: " + this.size());
     }
-
-    public void printHPFP_Queue(HPFP_Queue readyQueue){
-        for (PriorityQueue<Task> priorityQueue : readyQueue) {
-            System.out.println("Priority Queue Stats:  \n");
-            for (Task t : priorityQueue)
-            {
-                t.getPriority();
-            }
-        }
-    }
+//
+//    public void printHPFP_Queue(HPFP_Queue readyQueue){
+//        for (PriorityQueue<Task> priorityQueue : readyQueue) {
+//            System.out.println("Priority Queue Stats:  \n");
+//            for (Task t : priorityQueue)
+//            {
+//                t.getPriority();
+//            }
+//        }
+//    }
 
     public boolean isEmpty(ArrayList<PriorityQueue<Task>> readyQueue)
     {
@@ -45,22 +49,50 @@ public class HPFP_Queue extends ArrayList{
         return false;
     }
 
-    public void addTask(ArrayList<PriorityQueue<Task>> priorityQueue, Task t) {
-        priorityQueue.get(t.getPriority() - 1).add(t);
+    public void addTask(ArrayList<PriorityQueue<Task>> readyQueue, Task t)
+    {
+        readyQueue.get(t.getPriority() - 1).add(t);
+        // begin testing code
+        System.out.println("JUST ADDED A TASK: " + t.getName() + " " + t.getPriority());
+        int priorityQueueCounter = 1;
+
+        for (PriorityQueue<Task> priorityQueue : readyQueue)
+        {
+            System.out.println("Priority Queue Number: " + priorityQueueCounter);
+            if (priorityQueue.isEmpty())
+            {
+                System.out.println("EMPTY");
+            }
+            for (Task task : priorityQueue)
+            {
+                System.out.println("Name: " + task.getName() + "\tPriority: " + task.getPriority());
+            }
+            priorityQueueCounter += 1;
+        }
+        // end testing code
     }
 
-    public Task poll(ArrayList<PriorityQueue<Task>> readyQueue){
-        for (PriorityQueue<Task> priorityQueue : readyQueue){
-            if(!priorityQueue.isEmpty()){
-                try {
-                    return priorityQueue.poll();
+    public Task poll(ArrayList<PriorityQueue<Task>> readyQueue)
+    {
+        Task task = null;
+        for (PriorityQueue<Task> priorityQueue : readyQueue)
+        {
+            try
+            {
+                if (!priorityQueue.isEmpty())
+                {
+                    task = priorityQueue.poll();
                 }
-                catch (NullPointerException e){
-
+                else
+                {
+                    task = null;
                 }
             }
+            catch (NullPointerException e) {}
         }
-        return null;
+        return task;
     }
+
+
 
 }
