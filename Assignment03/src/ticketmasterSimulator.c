14f77833a601356e548533c9e53d1b5ecad6a954
Scot Matson <sjpmatson@gmail.com>
2016-06-30 20:45:41 -0700

    Update ticketmasterSimulator.c
    formatted this inside of github... so that wasn't fun. dbl check for accuracy. should be fine. now we just need everything else.. yay...

--- Assignment03/src/ticketmasterSimulator.c
+++ Assignment03/src/ticketmasterSimulator.c
@@ -1,99 +1,62 @@
 #include <stdio.h>
-
 #include <pthread.h>
 
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
-
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 // seller thread to serve one time slice (1 minute)
-
 void * sell(char *seller_type)
-
 {
+    While (having more work todo)
+    {
+        pthread_mutex_lock(&mutex);
+        pthread_cond_wait(&cond, &mutex);
+        pthread_mutex_unlock(&mutex);
 
-While (having more work todo)
+        // Serve any buyer available in this seller queue that is ready
+        // now to buy ticket till done with all relevant buyers in their queue
+        ..................
+    }
 
-{
-
-pthread_mutex_lock(&mutex);
-
-pthread_cond_wait(&cond, &mutex);
-
-pthread_mutex_unlock(&mutex);
-
-// Serve any buyer available in this seller queue that is ready
-
-// now to buy ticket till done with all relevant buyers in their queue
-
-..................
-
-}
-
-return NULL; // thread exits
-
+    return NULL; // thread exits
 }
 
 void wakeup_all_seller_threads()
-
 {
-
-pthread_mutex_lock(&mutex);
-
-pthread_cond_broadcast(&cond);
-
-pthread_mutex_unlock(&mutex);
-
+    pthread_mutex_lock(&mutex);
+    pthread_cond_broadcast(&cond);
+    pthread_mutex_unlock(&mutex);
 }
 
 int main()
-
 {
+    int i;
+    pthread_t tids[10];
+    char Seller_type;
 
-int i;
+    // Create necessary data structures for the simulator.
+    // Create buyers list for each seller ticket queue based on the
+    // N value within an hour and have them in the seller queue.
+    // Create 10 threads representing the 10 sellers.
+    seller-type = ‚ÄúH‚Äù;
+    pthread_create(&tids[i], NULL, sell, &seller-type);
+    seller-type = ‚ÄúM‚Äù;
 
-pthread_t tids[10];
+    for (i = 1; i < 4; i++)
+        pthread_create(&tids[i], NULL, sell, &seller-type);
 
-char Seller_type;
+    seller-type = ‚ÄúL‚Äù;
 
-// Create necessary data structures for the simulator.
+    for (i = 4; i < 10; i++)
+        pthread_create(&tids[i], NULL, sell, &seller-type);
 
-// Create buyers list for each seller ticket queue based on the
+    // wakeup all seller threads
+    wakeup_all_seller_threads();
 
-// N value within an hour and have them in the seller queue.
+    // wait for all seller threads to exit
+    for (i = 0 ; i < 10 ; i++)
+        Pthread_join(&tids[i], NULL);
 
-// Create 10 threads representing the 10 sellers.
-
-seller-type = ìHî;
-
-pthread_create(&tids[i], NULL, sell, &seller-type);
-
-seller-type = ìMî;
-
-for (i = 1; i < 4; i++)
-
-pthread_create(&tids[i], NULL, sell, &seller-type);
-
-seller-type = ìLî;
-
-for (i = 4; i < 10; i++)
-
-pthread_create(&tids[i], NULL, sell, &seller-type);
-
-// wakeup all seller threads
-
-wakeup_all_seller_threads();
-
-
-
-// wait for all seller threads to exit
-
-for (i = 0 ; i < 10 ; i++)
-
-Pthread_join(&tids[i], NULL);
-
-// Printout simulation results
-
-exit(0);
-
+    // Printout simulation results
+    exit(0);
 }
