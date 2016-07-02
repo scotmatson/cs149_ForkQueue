03d655e6aab2722abb2645e701df9cc205077e11
Daniel <mistahtam@gmail.com>
2016-07-02 16:21:12 -0700

    Added seller.h

---
+++ Assignment03/src/seller.h
@@ -0,0 +1,13 @@
+#ifndef tmsim.c
+#define tmsim.c
+
+struct seller {
+    char priority //H, M, or L seller
+    int responseTime // 1-10 minutes depending on priority
+    char customerQueue[20] // Customers in line with max size of 20 (arbitrary)
+
+
+
+}
+
+#endif
