1143c03bcf262e8d23ea2bfa70176e9970e9589c
Francisco McGee <franciscomcgee@gmail.com>
2016-07-03 15:49:29 -0700

    properly fixed customer.h, seller.h, ticket.h. Added gen_service_time to tmsimfran.c, but it doesn't seem to work as expected

--- Assignment03/src/ticket.h
+++ Assignment03/src/ticket.h
@@ -1,11 +1,12 @@
-#ifndef _ticket_h
-#define _ticket_h
+#ifndef TICKET_H_
+#define TICKET_H_
 
-typedef struct ticket {
-    int ticket_type;             // is this a H, M, or L ticket
-    char sold_by;                 // name of ticket seller: H1, M2, L3, etc
-    int seat_number;             // 
+struct ticket {
+    char seller_name[25];
+    char customer_name[25];
+    char* priority;;
+    int seat_number;
     bool availability;
 };
 
-#endif
+#endif // TICKET_H_
