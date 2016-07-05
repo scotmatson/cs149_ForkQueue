c2e61b000b6a76def03c9f37e51ee13dd7772bfd
Tyler Jones <tylerjones365@gmail.com>
2016-07-05 15:41:37 -0700

    new take on tmsim.c

---
+++ Assignment03/tests/tylers_test/sellers.h
@@ -0,0 +1,11 @@
+#ifndef SELLER_H
+#define SELLER_H
+struct Sellers {
+    char type; //Name of seller. E.G M1 or L3
+    int service_time;
+    PriorityQueue *ticket_line;
+    //double avg_service_time;
+    //char* priority; //Priority of seller. H, M, or L
+    //char customerQueue[200]; //Customer line for the seller
+};
+#endif /* SELLER_H */
