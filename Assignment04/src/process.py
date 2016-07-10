1feb36c56219cac4a0a1928b6f38dac863069d86
Francisco McGee <franciscomcgee@gmail.com>
2016-07-09 21:29:14 -0700

    process now has a list of pages

--- Assignment04/src/process.py
+++ Assignment04/src/process.py
@@ -4,11 +4,12 @@ import random
 
 class Process(object):
 
-	def __init__(self, name, size, arrival, duration):
+	def __init__(self, name, size, arrival, duration, pages):
 		self.name = name
 		self.size = size
 		self.arrival_time = arrival
 		self.duration = duration
+		self.pages = pages
 
 	def __lt__(self, other):
 		return self.arrival_time < other.arrival_time
