d6f97b3c3b63d38ca4c065a581ab5497beea1360
Francisco McGee <franciscomcgee@gmail.com>
2016-07-09 17:42:00 -0700

    small tweaks to the test_process

--- Assignment04/tests/test_process_generator.py
+++ Assignment04/tests/test_process_generator.py
@@ -1,22 +1,21 @@
+# this test creates processes from Process.py, then puts them into a set and prints out the attributes for each
 #from queue import PriorityQueue
 from Process import Process
 
 
 NUMBER_OF_PROCESSES = 150
 
-# make a set of 150 processes
-#pq = PriorityQueue(maxsize=0) # If maxsize is 0, queue size is infinite
-mySet = set()
+processSet = set()
+
+# make a set of 150 processes, add to processSet
 for x in range(NUMBER_OF_PROCESSES):
 	name = "P" + str(x)
 	process = Process(name)
 	#pq.put((process['arrival_time'], process))
-	mySet.add(process)
+	processSet.add(process)
 
-for process in mySet:
+for process in processSet:
 	print "Name: \t\t\t", process.name
 	print "Size: \t\t\t", process.size
 	print "Arrival Time: \t", process.arrival_time
 	print "Duration: \t\t", process.duration, "\n"
-
-
