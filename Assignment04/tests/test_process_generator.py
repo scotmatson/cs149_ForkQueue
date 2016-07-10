f048286601e85e7bb374b48c7d70e8dfd2949c70
Francisco McGee <franciscomcgee@gmail.com>
2016-07-09 17:38:33 -0700

    revised test_process_generator.py for Process.py. Process.py is in the wrong folder because I am fucking up the import

--- Assignment04/tests/test_process_generator.py
+++ Assignment04/tests/test_process_generator.py
@@ -1,21 +1,22 @@
-import random
-from queue import PriorityQueue
-import sys
-from .. import Process
+#from queue import PriorityQueue
+from Process import Process
 
-EXECUTION_TIME = 100
+
 NUMBER_OF_PROCESSES = 150
-PROCESS_NAME = range(11)
-PROCESS_SIZE = [5, 11, 17, 31]
-PROCESS_RUNTIME = range(1, 6)
 
 # make a set of 150 processes
-pq = PriorityQueue(maxsize=0) # If maxsize is 0, queue size is infinite
+#pq = PriorityQueue(maxsize=0) # If maxsize is 0, queue size is infinite
+mySet = set()
 for x in range(NUMBER_OF_PROCESSES):
 	name = "P" + str(x)
 	process = Process(name)
-	pq.put((process['arrival_time'], process))
+	#pq.put((process['arrival_time'], process))
+	mySet.add(process)
 
-while not pq.empty():
-    p = pq.get()
-    print(p)
+for process in mySet:
+	print "Name: \t\t\t", process.name
+	print "Size: \t\t\t", process.size
+	print "Arrival Time: \t", process.arrival_time
+	print "Duration: \t\t", process.duration, "\n"
+
+
