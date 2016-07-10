f9a7f5336c4a402665528edd769ff4980091e65a
Scot Matson <scot.matson@sjsu.edu>
2016-07-10 12:34:07 -0700

    This should be executable

--- Assignment04/src/memory/pagetable.py
+++ Assignment04/src/memory/pagetable.py
@@ -1,19 +1,27 @@
-try:
-    from memory import page
-except:
-    pass
+try: from memory import page
+except: pass
 
 try: import page
 except: pass
 
+
 class PageTable(object):
     '''
+    This class simulates the Page Table. The purpose of the Page Table is to make
+    user level processes believe that all of their data is in memory, which may
+    or may not be the case. Due to limitations on physical memory, some of the data
+    may have to reside on disk and be brought back into memory when it is accessed,
+    or "touched," by a process. Slots in physical memory are tracked using the
+    RAM_tokenCounter.
     '''
+    RAM = {} # RAM keys are strings page.name; value is the Process object itself
+    Disk = {}
+    RAM_tokenCounter = 100
+
     def __init__(self, number_of_pages, page_size):
         '''
         Arguments:
-            number_of_pages (int): The number of pages in the page list
-            page_size (int): The size of a page in MB
+            free_pages (int): The number of pages in the page list
         '''
         self.pages = dict()
         for i in range(number_of_pages):
@@ -33,10 +41,33 @@ class PageTable(object):
             The number of available pages in the page list
         '''
         return sum(page.access() == None for page in self.pages.values())
+        #return sum(x == None for x in self.free_pages.values())
 
     def get_free_pages(self, number_of_pages):
+        '''
+        Returns N number of free pages from the page table
+        '''
         free_pages = {}
         for page_id, page in self.pages.items():
             if self.pages[page_id].access() == None:
                 free_pages[page_id] = page
         return free_pages
+
+
+    # def touch(page):
+    #
+    #     page.lastAccessed = clock
+	#
+    #     # if the page is in memory, update its access and put it at the front of the queue in RAM; else,
+    #     # perform the algorithm
+    #     if page in RAM:
+    #         RAM.put(page)
+    #     else
+    #         pageReplacement(algorithm)
+    #
+    #
+
+    #def touch(self, page):
+    #    if page in self.RAM:
+    #        print "Adding to RAM: ", page.page_ID
+    #        self.RAM.add(page)
--- Assignment04/tests/test_process_generator.py
+++ Assignment04/tests/test_process_generator.py
@@ -1,10 +1,16 @@
 # this test creates processes from Process.py, then puts them into a set and prints out the attributes for each
 #from queue import PriorityQueue
-from Process import Process
-from queue import PriorityQueue
+from . import Process
+from Queue import PriorityQueue
 
 
 NUMBER_OF_PROCESSES = 150
+PROCESS_SIZES = [5, 11, 17, 31]
+MIN_DURATION = 1
+MAX_DURATION = 5
+
+MIN_ARRIVAL_TIME = 0
+MAX_ARRIVAL_TIME = 599
 
 processSet = set()
 pq = PriorityQueue()
@@ -26,3 +32,8 @@ print("\n\n\n\nPRINTING PQ")
 # test if PriorityQueue implemented comparison correctly by using "get" each item; they should increment in order
 for x in range (150):
 	print(pq.get().arrival_time)
+
+if __name__ == "__main__":
+    print("one.py is being run directly")
+else:
+    print("one.py is being imported into another module")
