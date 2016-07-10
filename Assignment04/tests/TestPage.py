2c29c07f5cdd6b99b3fb55347d84d2f6b36f6b28
Francisco McGee <franciscomcgee@gmail.com>
2016-07-10 13:07:00 -0700

    re-created TestClasses

---
+++ Assignment04/tests/TestPage.py
@@ -0,0 +1,9 @@
+class TestPage:
+    '''
+    A class which simulates pages used by memory
+    '''
+    def __init__(self, name, process_id, last_accessed):
+        self.name = name
+        self.process_id = process_id
+        self.last_accessed = last_accessed
+
---
+++ Assignment04/tests/TestProcess.py
@@ -0,0 +1,19 @@
+# by Francisco
+
+
+class Process:
+    # def __init__(self, name, size, arrival, duration):
+    #     self.name = name
+    #     self.size = size
+    #     self.arrival_time = arrival
+    #     self.duration = duration
+    #     self.pages = None
+
+    def __init__(self, name, arrival_time, duration):
+        self.name = name
+        self.arrival_time = arrival_time
+        self.duration = duration
+
+
+    def __lt__(self, other):
+        return self.arrival_time < other.arrival_time
