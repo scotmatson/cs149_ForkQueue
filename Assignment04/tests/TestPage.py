1a6b9d90a88ce4337323767707c2414278ea0496
Francisco McGee <franciscomcgee@gmail.com>
2016-07-10 18:16:29 -0700

    structure should be done now

--- Assignment04/tests/TestPage.py
+++ Assignment04/tests/TestPage.py
@@ -2,9 +2,9 @@ class Page:
     '''
     A class which simulates pages used by memory
     '''
-    def __init__(self, name, process_id, last_accessed, frequency):
+    def __init__(self, name, process_id, last_accessed):
         self.name = name
         self.process_id = process_id
         self.last_accessed = last_accessed
-        self.frequency = frequency
+        self.frequency = 0
 
