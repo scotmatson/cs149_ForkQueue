28f081e350b1642b431ed1d278f35bff6754b4b9
Scot Matson <scot.matson@sjsu.edu>
2016-07-10 20:21:55 -0700

    Adding documentation

--- Assignment04/src/memory/page.py
+++ Assignment04/src/memory/page.py
@@ -4,6 +4,14 @@ class Page(object):
     '''
 
     def __init__(self, name, process_id, last_accessed):
+        '''
+        Constructor function
+
+        Arguments:
+            name (string)       : Page identifier in form P[int]
+            process_id (int)    : Numerical identifier of stored process
+            last_accessed (int) : Time in execution when page was accessed
+        '''
         self.name = name
         self.process_id = process_id
         self.last_accessed = last_accessed
