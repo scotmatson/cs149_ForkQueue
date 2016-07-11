0afc3dcd018ff46529320740b59d6a95956e3eb3
Tyler Jones <tylerjones365@gmail.com>
2016-07-11 12:15:12 -0700

    first attempt at random_pick

--- Assignment04/src/algorithms/random.py
+++ Assignment04/src/algorithms/random.py
@@ -1,2 +1,15 @@
-def random():
-    pass
+'''
+This file includes implementation of the random_pick page replacement algorithm
+Solves CS149 Homework#4
+@author Tyler Jones
+'''
+def random(page_table):
+    #pick a random page in memory to evict
+    eviction_page_name = random.choice(page_table.memory.keys())
+    #Reset the page's frequency count to 0 because it got evicted
+    page_table.memory[eviction_page_name].frequency = 0
+    #Add the evicted page to disk
+    page_table.disk[eviction_page_name] = page_table.memory[eviction_page_name]
+    #Delete that page frm memory
+    del page_table.memory[eviction_page_name]
+
