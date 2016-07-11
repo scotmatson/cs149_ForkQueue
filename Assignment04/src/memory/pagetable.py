fc117c4d9f51a995f70147866b49abe83c6055db
Scot Matson <scot.matson@sjsu.edu>
2016-07-10 20:26:14 -0700

    pretty sure we are no longer using any of my code... hard not to feel a bit depressed about that.

--- Assignment04/src/memory/pagetable.py
+++ Assignment04/src/memory/pagetable.py
@@ -17,34 +17,15 @@ class PageTable(object):
     '''
 
     def __init__(self):
+        '''
+        Constructor function
+        '''
         self.disk = OrderedDict()
         self.memory = OrderedDict()
 
-    def available_pages(self):
-        '''
-        Returns the number of free pages remaining in the
-        page list as an int value. Empty pages are represented
-        by None type.
-
-        Returns:
-            The number of available pages in the page list
-        '''
-        return sum(page.access() == None for page in self.ram.values())
-
-    def get_free_pages(self, number_of_pages):
-        '''
-        Returns N number of free pages from the page table
-        '''
-        free_pages = {}
-        for page_id, page in self.ram.items():
-            if self.ram[page_id].access() == None and len(free_pages) < 4:
-                free_pages[page_id] = page
-                if len(free_pages) == 4:
-                    return free_pages
-        return None
-
     def touch(self, page):
         '''
+        TODO: Francisco needs to document this logic
         '''
         if self.memory.has_key(page.name):
             del self.memory[page.name]
