0d2f4f92e739d852d0589c93618cdec3c882a9fb
Scot Matson <scot.matson@sjsu.edu>
2016-07-19 06:40:20 -0700

    Comments, organizing

--- Assignment06/syscall.c
+++ Assignment06/syscall.c
@@ -1,6 +1,7 @@
 #define _POSIX_C_SOURCE 200809L
 #define PROCS 5             /* Number of processes (including parent)*/
 #define PIPES 2             /* Pipes per process(read/write) */
+#define MILLI 1000000000    /* Division needed to convert time to milliseconds */
 #include <stdio.h>          /* for printf() */
 #include <unistd.h>         /* for pipe(), fork(), and close() */
 #include <stdlib.h>         /* for exit(), srand(), rand() */
@@ -20,9 +21,9 @@ int main(int argc, char **argv) {
     FILE           *fh;            /* File Handler to write piped message */
 
     /* Process IDs */
-    pid_t          pid;            /* Control  filter for fork() */
-    pid_t          p_pid;          /* Parent proc */ 
-    pid_t          c_pid[PROCS];   /* Child procs */
+    pid_t          pid;          /* Control  filter for fork() */
+    pid_t          p_pid;        /* Parent proc */ 
+    pid_t          c_pid[PROCS]; /* Child procs */
 
     /* Time Management */
     static mach_timebase_info_data_t tb;
@@ -42,7 +43,7 @@ int main(int argc, char **argv) {
     fcntl(*p_fd, F_SETFL, O_NONBLOCK);
     pipe(p_fd);
 
-    /* Fork processes and setup child file descriptors */
+    /* Fork parent process and setup child file descriptors */
     struct fd_set socket;
     int fds[PROCS][PIPES];
     FD_ZERO(&socket);
@@ -54,12 +55,11 @@ int main(int argc, char **argv) {
             exit(1);
         } 
         else if (pid == 0) {
-            c_pid[i] = getpid();                 /* Store child process ID */
-            FD_SET(*fds[i], &socket);            /* Set FIFO File Descriptor control */
-            close(fds[i][0]);                    /* Close read side */
-            //fcntl(*fds[i], F_SETFL, O_NONBLOCK); /* Prevent blocking behavior */
-            pipe(fds[i]);                        /* Add File Descriptor to the pipe */
-            break;
+            c_pid[i] = getpid();      /* Store child process ID */
+            FD_SET(*fds[i], &socket); /* Set FIFO File Descriptor control */
+            close(fds[i][0]);         /* Close read side */
+            pipe(fds[i]);             /* Add File Descriptor to the pipe */
+            break;                    /* Break here, we don't want children looping */
         } 
         else {
             p_pid = getpid();
@@ -81,70 +81,68 @@ int main(int argc, char **argv) {
         /* Loop runs for 30 seconds (real time) */
         srand(time(NULL));
         if (getpid() == c_pid[0]) {
-            m0++;
             stop = mach_absolute_time();
             elapsed = (float)(stop-start) * tb.numer/tb.denom;
-            elapsed /= 1000000000;
+            elapsed /= MILLI;
+            m0++;
 
             /* Sending through pipe */
             char buf0[30];
             snprintf(buf0, sizeof(buf0), "0:%02f: Child 1 message %d\n", elapsed, m0);
-            printf("%s\n", buf0);
             write(*fds[0], buf0, sizeof(buf0));
         }    
         else 
         if (getpid() == c_pid[1]) {
-            m1++;
             stop = mach_absolute_time();
             elapsed = (float)(stop-start) * tb.numer/tb.denom;
-            elapsed /= 1000000000;
+            elapsed /= MILLI;
+            m1++;
+
             // Send through pipe
             char buf1[30];
             snprintf(buf1, sizeof(buf1), "0:%02f: Child 2 message %d\n", elapsed, m1);
-            printf("%s\n", buf1);
             write(*fds[1], buf1, sizeof(buf1));
         }
         else
         if (getpid() == c_pid[2]) {
-            m2++;
             stop = mach_absolute_time();
             elapsed = (float)(stop-start) * tb.numer/tb.denom;
-            elapsed /= 1000000000;
+            elapsed /= MILLI;
+            m2++;
+            
             // Send through pipe
             char buf2[30];
             snprintf(buf2, sizeof(buf2), "0:%02f: Child 3 message %d\n", elapsed, m2);
-            printf("%s\n", buf2);
             write(*fds[2], buf2, sizeof(buf2));
         }
         else
         if (getpid() == c_pid[3]) {
-            m3++;
             stop = mach_absolute_time();
             elapsed = (float)(stop-start) * tb.numer/tb.denom;
-            elapsed /= 1000000000;
+            elapsed /= MILLI;
+            m3++;
+
             // Send through pipe
             char buf3[30];
             snprintf(buf3, sizeof(buf3), "0:%02f: Child 4 message %d\n", elapsed, m3);
-            printf("%s\n", buf3);
             write(*fds[3], buf3, sizeof(buf3));
         }
         else
         if (getpid() == c_pid[4]) {
             //printf("Process %d is awaiting input...\n>> ", getpid());
-            m4++;
             stop = mach_absolute_time();
             elapsed = (float)(stop-start) * tb.numer/tb.denom;
-            elapsed /= 1000000000;
+            elapsed /= MILLI;
+            m4++;
 
-            //char msg5[20];
-            //scanf("%[^\n]%*c", msg5);
             // Send through pipe
+            //char msg5[30];
+            //scanf("%[^\n]%*c", msg5);
             //write(fd5[0], msg5, sizeof(msg5));
         }
         else {
-            // Parent
-            printf("Parent Process is %d\n", getpid());
-            retval = select(6, &socket, NULL, NULL, &timeout); /* Think this goes here */
+            /* Parent process */
+            retval = select(6, &socket, NULL, NULL, &timeout);
             switch(retval) {
                 case -1:
                     fprintf(stderr, "ERROR; Unable to select file descriptor\n");
@@ -153,10 +151,17 @@ int main(int argc, char **argv) {
                 case 1:
                     stop = mach_absolute_time();
                     elapsed = (float)(stop-start) * tb.numer/tb.denom;
-                    elapsed /= 1000000000;
+                    elapsed /= MILLI;
+
+                    /* 
+                    Need to prefix message with parent time,
+                    i.e., time - child message
+                          time - child message ...
+                    */
                     /* read proc data */
-                    read(p_fd[0], readBuffer, sizeof(readBuffer));
-                    fprintf(fh, "%s\n", readBuffer);
+                    read(p_fd[0], readBuffer, sizeof(readBuffer)); /* Not getting child message :( */
+                    printf("RB: %s\n", readBuffer);
+                    fprintf(fh, "%s\n", readBuffer); 
                     break;
                 default:
                     printf("No data to be read from the pipe\n");
@@ -164,7 +169,6 @@ int main(int argc, char **argv) {
             }
         }
         sleep(rand() % 3);
-        printf("Process %d waking up\n", getpid());
     }
 
     fclose(fh); 
